026da2b162b150431d0a938a96c163e2a90e0804
Rudolf <westfood@gmail.com>
2024-04-07 19:18:18 +0200

    skripty na logování

---
+++ ci/templates/exec.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Make Bash Great Again
+set -o errexit # exit when a command fails.
+set -o nounset # exit when using undeclared variables
+set -o pipefail # catch non-zero exit code in pipes
+# set -o xtrace # uncomment for bug hunting
+
+docker exec -it pywb-test-pywb-1 bash
