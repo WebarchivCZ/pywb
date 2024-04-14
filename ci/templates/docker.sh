82816b4fce165f1ba1079ac6a3290b06512e80d6
Rudolf <westfood@gmail.com>
2024-04-14 20:27:19 +0200

    Docker compose wrapper

---
+++ ci/templates/docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Make Bash Great Again
+set -o errexit # exit when a command fails.
+set -o nounset # exit when using undeclared variables
+set -o pipefail # catch non-zero exit code in pipes
+# set -o xtrace # uncomment for bug hunting
+
+sudo docker-compose -f {{ pywb_dir }}/docker-compose-test.yaml -p pywb-test "$@"
